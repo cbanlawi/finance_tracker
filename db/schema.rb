ecd880d3fe49a73a673d8dfbc56cb0aec3da9a78
cbanlawi <christine.banlawi@gmail.com>
2025-04-09 20:20:02 -0300

    (chore): add user reference to expenses table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_02_171715) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_231038) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -21,6 +21,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_02_171715) do
     t.date "date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_expenses_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -34,4 +36,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_02_171715) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "expenses", "users"
 end
