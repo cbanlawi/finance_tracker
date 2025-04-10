6ac56ef13cec5ccec1f0497caf0136eb84eb379d
cbanlawi <christine.banlawi@gmail.com>
2025-04-10 15:13:18 -0300

    (fix): refactor expense controller to scope by user and integrate authentication for request specs

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,18 +1,19 @@
 class ExpensesController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_expense, only: [ :show, :edit, :update, :destroy ]
+
   def index
-    @expenses = Expense.all
+    @expenses = current_user.expenses
   end
 
-  def show
-    @expense = Expense.find(params[:id])
-  end
+  def show; end
+
   def new
     @expense = Expense.new
   end
 
   def create
-    @expense = Expense.new(expense_params)
+    @expense = current_user.expenses.build(expense_params)
 
     if @expense.save
       redirect_to @expense, notice: "Expense was successfully created."
@@ -21,13 +22,9 @@ class ExpensesController < ApplicationController
     end
   end
 
-  def edit
-    @expense = Expense.find(params[:id])
-  end
+  def edit; end
 
   def update
-    @expense = Expense.find(params[:id])
-
     if @expense.update(expense_params)
       redirect_to @expense, notice: "Expense was successfully updated."
     else
@@ -36,13 +33,15 @@ class ExpensesController < ApplicationController
   end
 
   def destroy
-    @expense = Expense.find(params[:id])
     @expense.destroy
-
     redirect_to expenses_path, notice: "Expense was successfully deleted."
   end
 
   private
+
+  def set_expense
+    @expense = current_user.expenses.find(params[:id])
+  end
 
   def expense_params
     params.require(:expense).permit(:name, :amount, :category, :date)
