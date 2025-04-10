8a4c373266835b658df5ad8d20531dbf5529f450
cbanlawi <christine.banlawi@gmail.com>
2025-04-09 21:06:47 -0300

    (feat): associate Expense with User and add test coverage

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -1,4 +1,6 @@
 class Expense < ApplicationRecord
+  belongs_to :user
+
   validates :name, presence: true
   validates :amount, presence: true, numericality: { greater_than: 0 }
   validates :date, presence: true
